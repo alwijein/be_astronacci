b86b328f5f4a38a6d08895e0ef6b067499b0b63d
Alwi Jaya <alwijein@gmail.com>
2025-07-23 12:02:28 +0800

    fix: remove password confirmation requirement from registration validation

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -19,7 +19,7 @@ class AuthController extends Controller
             'name' => 'required|string|max:255',
             'phone' => 'nullable|string|max:15',
             'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:8|confirmed',
+            'password' => 'required|string|min:8',
         ]);
 
         if ($validator->fails()) {
